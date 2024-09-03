f96a91a32f7220dfd256e70cfe3bafec6488450e
guillermorobledo947 <guillermorobledo94@gmail.com>
2024-09-03 01:27:23 -0300

    primera subido

---
+++ Desafio1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Desafio_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Telefono telefono1 = new Telefono("a10", "Samsung")
+            {
+                numeroTelefonico = 1168700251,
+                codigoOperador = 1
+            };
+
+            Console.WriteLine("El modelo del telefono es un " + telefono1.getModelo() +
+                              ", su marca es " + telefono1.getMarca() +
+                              ", su numero es " + telefono1.numeroTelefonico +
+                              ", su codigo de Operador es " + telefono1.getCodigoOperador());
+            Console.WriteLine(telefono1.llamar("maxi"));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Desafio1/Telefono.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_1
+{
+    internal class Telefono
+    {
+        public Telefono(string modelo, string marca)
+        {
+            this.modelo = modelo;
+            this.marca = marca;
+        }
+
+        private string modelo;
+        private string marca;
+        public int numeroTelefonico { get; set; }
+        public int codigoOperador { get; set; }
+
+        public void setModelo(string Modelo)
+        {
+            modelo = Modelo;
+        }
+
+        public void setMarca(string Marca)
+        {
+            marca = Marca;
+        }
+
+        public string getModelo()
+        {
+            return modelo;
+        }
+
+        public string getMarca()
+        {
+            return marca;
+        }
+
+        public void setCodigoOperador(int CodigoOperador)
+        {
+            codigoOperador = CodigoOperador;
+        }
+
+        public int getCodigoOperador()
+        {
+            if (codigoOperador == 1 || codigoOperador == 2 || codigoOperador == 3)
+                return codigoOperador;
+            else
+                return 0;
+        }
+
+        public string llamar()
+        {
+            return "realizando llamada";
+        }
+        public string llamar( string contacto)
+        {
+            return "llamando a " + contacto;
+        }
+    }
+}
